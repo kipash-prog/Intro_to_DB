a5d433f9c9bcdf1a805130444ac1c77e5440301c
kipash-prog <kidus.ugr-0110-15@aau.edu.et>
2024-10-17 16:57:13 +0300

    connection checker

---
+++ MySQLServer.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+    # Establish the connection
+conn = mysql.connector.connect(
+        host="127.0.0.1",
+        user="root",
+        password="@147896AB"
+    )
+
+mycursor = conn.cursor()
+try:
+    if mycursor.execute("create database if not exists alx_book_store"):
+        print(f"Database 'alx_book_store' created successfully!")
+except mysql.conncetor.Error as e:
+    print(f"Error: {e}")
+
+conn.close()
+mycursor.close()
+        
+
+    
