9f643e3cdac6c085e24ac488c7dcfb12fcead45b
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-10-21 15:11:24 +0300

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -34,6 +34,7 @@ CREATE TABLE Orders_Details (
     orderDetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
+    quantity DOUBLE ,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
