c99420a739a4c4bf6881463c809bbd42edef4fdf
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-10-21 15:17:04 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -30,9 +30,10 @@ CREATE TABLE Orders (
 );
 
 CREATE TABLE Orders_Details (
-    orderDetailid INT PRIMARY KEY,
+    orderDetails INT PRIMARY KEY,
     order_id INT,
     book_id INT,
+    quantity DOUBLE ,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
